a926dbc9938ef55d9f70660339d9e39b2dfe7a72
Dawid <sterenczak.dawid@gmail.com>
2023-12-27 20:33:58 +0100

    testy dawid s

--- src/test/java/seleniumtests/FrontendTests.java
+++ src/test/java/seleniumtests/FrontendTests.java
@@ -155,6 +155,10 @@ public class FrontendTests {
 		// Sprawdź, czy element o id cart-product istnieje
 		WebElement cartProduct = driver.findElement(By.id("cart-product"));
 		Assertions.assertNotNull(cartProduct, "Cart product does not exist.");
+
+		// Czyszczenie koszyka
+		driver.findElement(By.id("cart-icon")).click();
+		driver.findElement(By.id("delete-from-cart")).click();
 	}
 
 	@Test
@@ -173,6 +177,10 @@ public class FrontendTests {
 
 		// Sprawdź, czy liczba elementów w koszyku to ' 3 '
 		Assertions.assertEquals("3", cartItemsCount.getText().trim(), "Incorrect number of items in the cart.");
+
+		// Czyszczenie koszyka
+		driver.findElement(By.id("cart-icon")).click();
+		driver.findElement(By.id("delete-from-cart")).click();
 	}
 
 	@Test
@@ -346,6 +354,10 @@ public class FrontendTests {
 
 		// Kliknij pierwszy przycisk o klasie delete-admin-item-button
 		driver.findElement(By.className("delete-admin-item-button")).click();
+
+		// Czyszczenie koszyka
+		driver.findElement(By.id("cart-icon")).click();
+		driver.findElement(By.id("delete-from-cart")).click();
 	}
 
 	@Test
@@ -386,6 +398,11 @@ public class FrontendTests {
 
 		// Sprawdź informacje o drugim produkcie w koszyku
 		checkCartItemInformation(secondProductName, 3, 1);
+
+		// Czyszczenie koszyka
+		driver.findElement(By.id("cart-icon")).click();
+		driver.findElement(By.id("delete-from-cart")).click();
+		driver.findElement(By.id("delete-from-cart")).click();
 	}
 
 	private void checkCartItemInformation(String productName, Integer quantity, int index) {
@@ -403,4 +420,349 @@ public class FrontendTests {
 		// Kliknij przycisk o id home-link
 		driver.findElement(By.id("home-link")).click();
 	}
+
+	@Test
+	public void shouldDisableCreateOrderButtonWhenOrderIsEmpty() {
+		driver.get(URL);
+
+		// Click on the cart icon
+		WebElement cartIcon = driver.findElement(By.id("cart-icon"));
+		cartIcon.click();
+
+		// Assert that the create order button exists
+		WebElement createOrderButton = driver.findElement(By.id("create-order"));
+		Assertions.assertTrue(createOrderButton.isDisplayed());
+
+		// Assert that the create order button is disabled
+		Assertions.assertFalse(createOrderButton.isEnabled());
+	}
+
+	@Test
+	public void shouldDisplayTotalPriceOfItemsAddedToCart() {
+		// Otwórz stronę
+		driver.get(URL);
+
+		// Kliknij przycisk o id admin-link
+		driver.findElement(By.id("admin-link")).click();
+
+		// Kliknij przycisk o klasie addLink
+		driver.findElement(By.className("addLink")).click();
+
+		// Wprowadź nazwę produktu
+		driver.findElement(By.id("name-input")).sendKeys("Aaaaaa");
+
+		// Wprowadź ilość produktu
+		WebElement stockQuantityInput = driver.findElement(By.id("stockQuantity-input"));
+		stockQuantityInput.clear();
+		stockQuantityInput.sendKeys("420");
+
+		// Wprowadź cenę produktu
+		WebElement priceInput = driver.findElement(By.id("price-input"));
+		priceInput.clear();
+		priceInput.sendKeys("120");
+
+		// Kliknij przycisk o id form-submit-button
+		driver.findElement(By.id("form-submit-button")).click();
+
+		// Kliknij przycisk o id home-link
+		driver.findElement(By.id("home-link")).click();
+
+		// Kliknij przycisk o klasie add__button trzy razy
+		for (int i = 0; i < 3; i++) {
+			driver.findElement(By.className("add__button")).click();
+		}
+
+		// Kliknij przycisk o id cart-icon
+		driver.findElement(By.id("cart-icon")).click();
+
+		// Sprawdź, czy element o id total-price istnieje
+		WebElement totalPriceElement = driver.findElement(By.id("total-price"));
+		Assertions.assertNotNull(totalPriceElement, "Total price element does not exist.");
+
+		// Sprawdź, czy cena wynosi 360,00 zł
+		Assertions.assertEquals("= 360,00 zł", totalPriceElement.getText(), "Total price is not as expected.");
+
+		// Czyszczenie zmian
+		driver.findElement(By.id("admin-link")).click();
+		driver.findElement(By.cssSelector(".delete-admin-item-button")).click();
+		// Czyszczenie koszyka
+		driver.findElement(By.id("cart-icon")).click();
+		driver.findElement(By.id("delete-from-cart")).click();
+	}
+
+	@Test
+	public void shouldDisableAddingNewProductWhenPriceProvidedIsIncorrect() {
+		// Otwórz stronę
+		driver.get(URL);
+
+		// Kliknij przycisk o id admin-link
+		driver.findElement(By.id("admin-link")).click();
+
+		// Kliknij przycisk o klasie addLink
+		driver.findElement(By.className("addLink")).click();
+
+		// Wprowadź nazwę produktu
+		driver.findElement(By.id("name-input")).sendKeys("new product");
+
+		// Wyczyść pole z ilością i wprowadź wartość
+		WebElement stockQuantityInput = driver.findElement(By.id("stockQuantity-input"));
+		stockQuantityInput.clear();
+		stockQuantityInput.sendKeys("420");
+
+		// Wyczyść pole z ceną
+		driver.findElement(By.id("price-input")).clear();
+
+		// Kliknij w dowolne miejsce na stronie, aby schować klawiaturę lub inne aktywne elementy
+		driver.findElement(By.tagName("body")).click();
+
+		// Sprawdź, czy przycisk o id form-submit-button jest wyłączony
+		WebElement formSubmitButton = driver.findElement(By.id("form-submit-button"));
+		Assertions.assertFalse(formSubmitButton.isEnabled(), "Form submit button should be disabled.");
+
+		// Sprawdź, czy element o id wrong-price istnieje
+		WebElement wrongPriceElement = driver.findElement(By.id("wrong-price"));
+		Assertions.assertNotNull(wrongPriceElement, "Wrong price element should exist.");
+	}
+
+	@Test
+	public void shouldDisableAddingNewProductWhenStockQuantityProvidedIsIncorrect() {
+		// Otwórz stronę
+		driver.get(URL);
+
+		// Kliknij przycisk o id admin-link
+		driver.findElement(By.id("admin-link")).click();
+
+		// Kliknij przycisk o klasie addLink
+		driver.findElement(By.className("addLink")).click();
+
+		// Wprowadź nazwę produktu
+		driver.findElement(By.id("name-input")).sendKeys("new product");
+
+		// Wyczyść pole z ilością i wprowadź wartość
+		WebElement stockQuantityInput = driver.findElement(By.id("stockQuantity-input"));
+		stockQuantityInput.clear();
+		stockQuantityInput.sendKeys("0");
+
+		// Wprowadź cenę produktu
+		driver.findElement(By.id("price-input")).sendKeys("50.89");
+
+		// Sprawdź, czy przycisk o id form-submit-button jest wyłączony
+		WebElement formSubmitButton = driver.findElement(By.id("form-submit-button"));
+		Assertions.assertFalse(formSubmitButton.isEnabled(), "Form submit button should be disabled.");
+
+		// Sprawdź, czy element o id wrong-quantity istnieje
+		WebElement wrongQuantityElement = driver.findElement(By.id("wrong-quantity"));
+		Assertions.assertNotNull(wrongQuantityElement, "Wrong quantity element should exist.");
+	}
+
+	@Test
+	public void shouldCorrectlyAddToCartProductsFromDifferentPages() {
+		// Otwórz stronę
+		driver.get(URL);
+		driver.findElement(By.id("home-link")).click();
+
+		// Dodaj pierwszy produkt do koszyka
+		String firstProductName = driver.findElements(By.className("product__title")).get(0).getText();
+		WebElement amountInputFirst = driver.findElements(By.className("amount__input")).get(0);
+		amountInputFirst.clear();
+		amountInputFirst.sendKeys("2");
+		driver.findElements(By.className("add__button")).get(0).click();
+
+		// Przejdź do następnej strony
+		driver.findElement(By.id("next-page-button")).click();
+
+		// Dodaj drugi produkt do koszyka
+		String secondProductName = driver.findElements(By.className("product__title")).get(1).getText();
+		WebElement amountInputSecond = driver.findElements(By.className("amount__input")).get(1);
+		amountInputSecond.clear();
+		amountInputSecond.sendKeys("3");
+		driver.findElements(By.className("add__button")).get(1).click();
+
+		// Sprawdź liczbę produktów w koszyku
+		WebElement cartItemsCount = driver.findElement(By.className("cart__items__count"));
+		Assertions.assertEquals("5", cartItemsCount.getText().trim(), "Incorrect number of items in the cart.");
+
+		// Powróć do poprzedniej strony i sprawdź pierwszy produkt w koszyku
+		driver.findElement(By.id("previous-page-button")).click();
+		WebElement firstCartItem = driver.findElements(By.className("cart__button")).get(0);
+		firstCartItem.click();
+		WebElement firstCartItemHeader = driver.findElements(By.className("cart__item__header")).get(0);
+		Assertions.assertEquals(firstProductName, firstCartItemHeader.getText(), "Incorrect product name in the cart.");
+
+		driver.findElement(By.id("home-link")).click();
+		// Powróć do następnej strony i sprawdź drugi produkt w koszyku
+		driver.findElement(By.id("next-page-button")).click();
+		WebElement secondCartItem = driver.findElements(By.className("cart__button")).get(0);
+		secondCartItem.click();
+		WebElement secondCartItemHeader = driver.findElements(By.className("cart__item__header")).get(1);
+		Assertions.assertEquals(secondProductName, secondCartItemHeader.getText(), "Incorrect product name in the cart.");
+
+		// Czyszczenie koszyka
+		driver.findElement(By.id("cart-icon")).click();
+		driver.findElement(By.id("delete-from-cart")).click();
+		driver.findElement(By.id("delete-from-cart")).click();
+	}
+
+	@Test
+	public void shouldDisableEditingProductWhenStockQuantityProvidedIsNegative() {
+		// Otwórz stronę
+		driver.get(URL);
+
+		// Kliknij przycisk o id admin-link
+		driver.findElement(By.id("admin-link")).click();
+
+		// Kliknij pierwszy przycisk edycji produktu o klasie product__admin__item__edit
+		driver.findElements(By.className("product__admin__item__edit")).get(0).click();
+
+		// Wyczyść pole z ilością i wprowadź wartość ujemną
+		WebElement stockQuantityInput = driver.findElement(By.id("stockQuantity-input"));
+		stockQuantityInput.clear();
+		stockQuantityInput.sendKeys("-1");
+
+		// Kliknij w dowolne miejsce na stronie, aby schować klawiaturę lub inne aktywne elementy
+		driver.findElement(By.tagName("body")).click();
+
+		// Sprawdź, czy przycisk o id form-submit-button jest wyłączony
+		WebElement formSubmitButton = driver.findElement(By.id("form-submit-button"));
+		Assertions.assertFalse(formSubmitButton.isEnabled(), "Form submit button should be disabled.");
+
+		// Sprawdź, czy element o id wrong-quantity istnieje
+		WebElement wrongQuantityElement = driver.findElement(By.id("wrong-quantity"));
+		Assertions.assertNotNull(wrongQuantityElement, "Wrong quantity element should exist.");
+	}
+
+	// Assuming this is inside the FrontendTests class
+
+	@Test
+	public void shouldProperlyEditProductName() {
+		// Otwórz stronę
+		driver.get(URL);
+		driver.findElement(By.id("home-link")).click();
+
+		// Pobierz i edytuj nazwę produktu
+		String firstProductName = driver.findElements(By.className("product__title")).get(0).getText();
+		driver.findElement(By.id("admin-link")).click();
+		WebElement productAdminItemHeader = driver.findElements(By.className("header__product__admin__item")).get(0);
+		Assertions.assertEquals(firstProductName, productAdminItemHeader.getText(), "Incorrect product name in admin.");
+		driver.findElement(By.className("product__admin__item__edit")).click();
+		driver.findElement(By.id("name-input")).sendKeys(" XXX");
+		driver.findElement(By.id("form-submit-button")).click();
+		driver.findElement(By.id("home-link")).click();
+
+		WebElement modifiedProductTitle = driver.findElements(By.className("product__title")).get(0);
+		Assertions.assertEquals(firstProductName + " XXX", modifiedProductTitle.getText(), "Product name not properly edited.");
+
+		// Czyszczenie
+		driver.findElement(By.id("admin-link")).click();
+		driver.findElement(By.className("product__admin__item__edit")).click();
+		driver.findElement(By.id("name-input")).clear();
+		driver.findElement(By.id("name-input")).sendKeys(firstProductName);
+		driver.findElement(By.id("form-submit-button")).click();
+		driver.findElement(By.id("home-link")).click();
+	}
+
+	@Test
+	public void shouldProperlyEditProductQuantity() {
+		driver.get(URL);
+		driver.findElement(By.id("admin-link")).click();
+
+		String firstProductQuantity = driver.findElements(By.className("product__admin__amount")).get(0).getText();
+		String cleanedQuantity = firstProductQuantity.replace("Ilość: ", "");
+
+		WebElement productAdminAmount = driver.findElements(By.className("product__admin__amount")).get(0);
+		Assertions.assertEquals(firstProductQuantity, productAdminAmount.getText(), "Incorrect product quantity in admin.");
+
+		driver.findElements(By.className("product__admin__item__edit")).get(0).click();
+		driver.findElement(By.id("stockQuantity-input")).clear();
+		driver.findElement(By.id("stockQuantity-input")).sendKeys("420");
+		driver.findElement(By.id("form-submit-button")).click();
+		driver.findElement(By.id("home-link")).click();
+
+		driver.findElement(By.id("admin-link")).click();
+		WebElement modifiedProductAdminAmount = driver.findElements(By.className("product__admin__amount")).get(0);
+		Assertions.assertEquals("Ilość: 420", modifiedProductAdminAmount.getText(), "Product quantity not properly edited.");
+
+		// Czyszczenie
+		driver.findElement(By.id("admin-link")).click();
+		driver.findElements(By.className("product__admin__item__edit")).get(0).click();
+		driver.findElement(By.id("stockQuantity-input")).clear();
+		driver.findElement(By.id("stockQuantity-input")).sendKeys(cleanedQuantity);
+		driver.findElement(By.id("form-submit-button")).click();
+		driver.findElement(By.id("home-link")).click();
+	}
+
+	@Test
+	public void shouldProperlyEditProductPrice() {
+		driver.get(URL);
+		driver.findElement(By.id("home-link")).click();
+
+		String firstProductPrice = driver.findElements(By.className("product__price")).get(0).getText();
+
+		driver.findElement(By.id("admin-link")).click();
+
+		WebElement productAdminPrice = driver.findElements(By.className("product__admin__price")).get(0);
+		Assertions.assertEquals(firstProductPrice, productAdminPrice.getText(), "Incorrect product price in admin.");
+
+		driver.findElements(By.className("product__admin__item__edit")).get(0).click();
+		driver.findElement(By.id("price-input")).clear();
+		driver.findElement(By.id("price-input")).sendKeys("100");
+		driver.findElement(By.id("form-submit-button")).click();
+		driver.findElement(By.id("home-link")).click();
+
+		WebElement modifiedProductPrice = driver.findElements(By.className("product__price")).get(0);
+		Assertions.assertEquals("100,00 zł", modifiedProductPrice.getText(), "Product price not properly edited.");
+
+		// Czyszczenie
+		String priceValue = firstProductPrice.split(",")[0].trim();
+		driver.findElement(By.id("admin-link")).click();
+		driver.findElements(By.className("product__admin__item__edit")).get(0).click();
+		driver.findElement(By.id("price-input")).clear();
+		driver.findElement(By.id("price-input")).sendKeys(priceValue);
+		driver.findElement(By.id("form-submit-button")).click();
+		driver.findElement(By.id("home-link")).click();
+	}
+
+	@Test
+	public void shouldEditingPriceValueNotImpactOnOrders() {
+		driver.get(URL);
+		driver.findElement(By.id("home-link")).click();
+
+		String firstProductName = driver.findElements(By.className("product__title")).get(0).getText();
+		String firstProductPrice = driver.findElements(By.className("product__price")).get(0).getText();
+
+		driver.findElements(By.className("add__button")).get(0).click();
+
+		driver.findElement(By.id("cart-icon")).click();
+		driver.findElement(By.id("create-order")).click();
+
+		driver.findElement(By.id("orders-link")).click();
+		WebElement orderItemName = driver.findElements(By.className("order__item__name")).get(0);
+		WebElement orderItemPrice = driver.findElements(By.className("order__item__price")).get(0);
+
+		Assertions.assertEquals(firstProductName, orderItemName.getText(), "Incorrect product name in order.");
+		Assertions.assertEquals(firstProductPrice.trim(), orderItemPrice.getText().trim(), "Incorrect product price in order.");
+
+		driver.findElement(By.id("admin-link")).click();
+		driver.findElements(By.className("product__admin__item__edit")).get(0).click();
+
+		driver.findElement(By.id("price-input")).clear();
+		driver.findElement(By.id("price-input")).sendKeys("20");
+		driver.findElement(By.id("form-submit-button")).click();
+
+		driver.findElement(By.id("orders-link")).click();
+
+		orderItemName = driver.findElements(By.className("order__item__name")).get(0);
+		orderItemPrice = driver.findElements(By.className("order__item__price")).get(0);
+
+		Assertions.assertEquals(firstProductName, orderItemName.getText(), "Product name in order should remain unchanged.");
+		Assertions.assertEquals(firstProductPrice.trim(), orderItemPrice.getText().trim(), "Product price in order should remain unchanged.");
+
+		driver.findElement(By.id("admin-link")).click();
+		driver.findElements(By.className("product__admin__item__edit")).get(0).click();
+
+		driver.findElement(By.id("price-input")).clear();
+		driver.findElement(By.id("price-input")).sendKeys("39");
+		driver.findElement(By.id("form-submit-button")).click();
+	}
+
 }
